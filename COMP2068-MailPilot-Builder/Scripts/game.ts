27609b4c535732e6dfe17dd8cff89db8ba89daba
Patrick Richey <patrick_richey@hotmail.com>
2014-10-30 10:56:00 -0400

    MailPilot Builder baseline structure

---
+++ COMP2068-MailPilot-Builder/Scripts/game.ts
@@ -0,0 +1,34 @@
+﻿var stage: createjs.Stage;
+var queue;
+
+function preload(): void {
+    queue = new createjs.LoadQueue();
+    queue.installPlugin(createjs.Sound);
+    queue.addEventListener("complete", init);
+    queue.loadManifest([
+        { id: "loading", src: "images/loading.jpg" },
+        { id: "yay", src: "sounds/yay.ogg" }
+    ]);
+}
+
+function init(): void {
+    stage = new createjs.Stage(document.getElementById("canvas"));
+    stage.enableMouseOver(20);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    gameStart();
+}
+
+function gameLoop(event):void {
+    stage.update();
+}
+
+class Plane {
+    constructor() {
+    }
+}
+
+function gameStart(): void {
+   
+
+}
